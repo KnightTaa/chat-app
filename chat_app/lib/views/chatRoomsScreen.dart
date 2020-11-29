dd396926d07c538ef4f20018e3d7dce2e71eca92
KnightTaa <74761059+KnightTaa@users.noreply.github.com>
2020-11-30 02:01:41 +0530

    login and sign up UI and FireAuth Config

---
+++ chat_app/lib/views/chatRoomsScreen.dart
@@ -0,0 +1,34 @@
+import 'package:chat_app/services/auth.dart';
+import 'package:chat_app/widgets/widgets.dart';
+import 'package:flutter/material.dart';
+
+class ChatRoom extends StatefulWidget {
+  @override
+  _ChatRoomState createState() => _ChatRoomState();
+}
+
+class _ChatRoomState extends State<ChatRoom> {
+
+  AuthMethods authMethods = new AuthMethods();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Image.asset("assets/images/logo.png",
+        height: 50,),
+        actions: [
+          GestureDetector(
+            onTap: (){
+              authMethods.signOut();
+              Navigator.pushReplacement(context, newRoute)
+            },
+            child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 16),
+                child: Icon(Icons.exit_to_app)),
+          )
+        ],
+      ),
+    );
+  }
+}
